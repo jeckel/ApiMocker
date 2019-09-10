f48fbdef7d70e028e48a010dc1febe538d332dc8
Jeckel <devci@j3ck3l.me>
2019-09-10 16:08:12 +0200

    Initialize tests

---
+++ tests/RouteTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\App;
+
+use App\Route;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class RouteTest
+ */
+final class RouteTest extends TestCase
+{
+
+    /**
+     * @test getUri
+     */
+    public function testGetUri()
+    {
+    }
+
+    /**
+     * @test toArray
+     */
+    public function testToArray()
+    {
+    }
+
+    /**
+     * @test getMethod
+     */
+    public function testGetMethod()
+    {
+    }
+
+    /**
+     * @test getResponseCode
+     */
+    public function testGetResponseCode()
+    {
+    }
+
+    /**
+     * @test getResponse
+     */
+    public function testGetResponse()
+    {
+    }
+}
